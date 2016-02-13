abc2635e7a48a4bee5f6ff5c33f8d48b8990476d
fcwg <fcwong@yahoo.com>
2016-02-13 03:39:05 -0800

    fix makeup form, add locations, add promo.html

--- php/send-contact.php
+++ php/send-contact.php
@@ -1,17 +1,20 @@
 <?php
 
-	$name = @trim(stripslashes($_POST['name'])); 
+	$student_name = @trim(stripslashes($_POST['student_name'])); 
 	$email = @trim(stripslashes($_POST['email']));
-	$subject = @trim(stripslashes($_POST['subject']));
-	$website = @trim(stripslashes($_POST['website']));
-	$message = @trim(stripslashes($_POST['message'])); 
+	$phone = @trim(stripslashes($_POST['phone']));
+	$class_name = @trim(stripslashes($_POST['class_name']));
+	$parent_name = @trim(stripslashes($_POST['parent_name'])); 
+	$class_location = @trim(stripslashes($_POST['class_location'])); 
+	$date_missed = @trim(stripslashes($_POST['date_missed'])); 
+	$makeup_date = @trim(stripslashes($_POST['makeup_date'])); 
 
 	$email_from = $email;
-	$email_to = 'fcwong@tompkinssport.com';//replace with your email
+	$email_to = 'cherie@tompkinssport.com';//replace with your email
 
-	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Website: ' . $website . "\n\n" . 'Message: ' . $message;
+	$body = 'Student Name: ' . $student_name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Phone: ' . $phone . "\n\n" . 'Class Name: ' . $class_name . "\n\n" . 'Parent Name: ' . $parent_name . "\n\n" . 'Location: ' . $class_location . "\n\n" . 'Date Missed: ' . $date_missed . "\n\n" . 'Preferred Makeup Date: ' . $makeup_date;
 
-	$success = @mail($email_to, $body, 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Website: ' . $website . "\n\n" . 'Message: ' . $message);
+	$success = @mail($email_to, $body, 'Student Name: ' . $student_name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Phone: ' . $phone . "\n\n" . 'Class Name: ' . $class_name . "\n\n" . 'Parent Name: ' . $parent_name . "\n\n" . 'Location: ' . $class_location . "\n\n" .  'Date Missed: ' . $date_missed . "\n\n" . 'Preferred Makeup Date: ' . $makeup_date);
 	
 ?>
 
@@ -19,5 +22,5 @@
 <html lang="en-US">
 <head>
 	<script>alert("Thank you for contacting us. We value your business and will be in touch shortly!");</script>
-	<meta HTTP-EQUIV="REFRESH" content="0; url=../contact.html"> 
+	<meta HTTP-EQUIV="REFRESH" content="0; url=../promo.html"> 
 </head>
