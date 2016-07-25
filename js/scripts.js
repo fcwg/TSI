5f07915724295567bd166cf8f8de0e5921a6ce70
fcwg <fcwong@yahoo.com>
2016-07-24 18:58:23 -0700

    fnl

--- js/scripts.js
+++ js/scripts.js
@@ -250,7 +250,7 @@ dragable: true
         $('li a[href*=#]').bind("click", function(e){
             var anchor = $(this);
             $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top -210
+                scrollTop: $(anchor.attr('href')).offset().top -110
             }, 1000);
             e.preventDefault();
         });
@@ -302,6 +302,19 @@ dragable: true
 		}	
 		parallaxInit();
 	}());
+
+	// SubMenu
+
+// 	(function($){
+// 	$(document).ready(function(){
+// 		$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+// 			event.preventDefault(); 
+// 			event.stopPropagation(); 
+// 			$(this).parent().siblings().removeClass('open');
+// 			$(this).parent().toggleClass('open');
+// 		});
+// 	});
+// })(jQuery);
 
 	
 
